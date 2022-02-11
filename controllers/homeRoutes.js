6235c74f800ac3456139201aa407997c5bd8e944
Ernesto Tham <ernesto.tham@kbslp.com>
2022-02-11 01:11:45 -0500

    testing authentication

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,12 +24,17 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/login');
+  //   return;
+  // }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+
+  res.render('signup');
 });
 
 module.exports = router;
