6bb388400f12ebb01b92ae564035cf61cdb63ad2
Ernesto Tham <ernesto.tham@kbslp.com>
2022-02-12 19:07:22 -0500

    checkpoint before merging branch

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const oidcRoutes = require('./userOidcRoutes');
+const NFTRoutes = require('./NFTRoutes');
 
 router.use('/users', userRoutes);
 router.use('/oidc', oidcRoutes);
+router.use('/nft', NFTRoutes);
 
 module.exports = router;
