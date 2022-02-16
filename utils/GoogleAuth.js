9d635b4c3610d0ede3ad589191f81dca15c94d12
Ernesto Tham <ernestotha@gmail.com>
2022-02-15 20:15:35 -0500

    Update GoogleAuth.js

--- utils/GoogleAuth.js
+++ utils/GoogleAuth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://bitfitted.com/api/auth/google/callback",
+    callbackURL: "http://bitfitted.herokuapp.com/api/auth/google/callback",
     passReqToCallback : true
 },
 function(request, accessToken, refreshToken, profile, done){
