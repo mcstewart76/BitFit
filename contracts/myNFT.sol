0fe4beeac8c44261b29e226ecf528dac567634d3
Mike Kelly <mike.forge@gmail.com>
2022-02-16 12:29:11 -0500

    adjusted hardhat config for creating tests to ensure the minting process will function recursively

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -1,4 +1,4 @@
-//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
+//Contract based on https://docs.openzeppelin.com/contracts/4.x/erc721
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
@@ -9,8 +9,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MyNFT") {
+
+    uint256 public tokenCounter;
+    mapping (uint256 => string) private _tokenURIs;
+
+    constructor(string memory name,
+    string memory symbol
+    ) ERC721(name, symbol) {
+        tokenCounter = 0;
     }
+
+function mint(string memory _tokenURI) public {
+    _safeMint(msg.sender, tokenCounter);
+    _setTokenURI(tokenCounter, _tokenURI);
+
+    tokenCounter++;
 }
 
+function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal virtual {
+    require(
+        _exists(_tokenId),
+        "ERC721Metadata: URI set of nonexistent token"
+    );  // Checks if the tokenId exists
+    _tokenURIs[_tokenId] = _tokenURI;
+}
 
+function tokenURI(uint256 _tokenId) public view virtual override returns(string memory) {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
+        return _tokenURIs[_tokenId];
+    }
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,16 +2,31 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
  require('dotenv').config();
+ require("@nomiclabs/hardhat-waffle");
  require("@nomiclabs/hardhat-ethers");
- const { ALCHEMY_URL, METAMASK_KEY } = process.env;
- module.exports = {
-    solidity: "0.8.1",
-    defaultNetwork: "ropsten",
-    networks: {
-       hardhat: {},
-       ropsten: {
-          url: ALCHEMY_URL,
-          accounts: [`0x${METAMASK_KEY}`]
-       }
-    },
- }
+ require("@nomiclabs/hardhat-etherscan");
+
+ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+//  const { ALCHEMY_URL, METAMASK_KEY } = process.env;
+//  module.exports = {
+//     solidity: "0.8.1",
+//     defaultNetwork: "ropsten",
+//     networks: {
+//        hardhat: {},
+//        ropsten: {
+//           url: ALCHEMY_URL,
+//           accounts: [`0x${METAMASK_KEY}`]
+//        }
+//     },
+//  }
+
+module.exports = {
+  solidity: "0.8.1",
+};
