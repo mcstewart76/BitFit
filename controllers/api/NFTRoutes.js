ec56dbc40bd4d935d3d592d3177476e273299178
Ernesto Tham <ernesto.tham@kbslp.com>
2022-02-16 23:14:52 -0500

    updating APi routes

--- controllers/api/NFTRoutes.js
+++ controllers/api/NFTRoutes.js
@@ -4,29 +4,68 @@ const { NftAttributes } = require('../../models');
 const { Nftitems } = require('../../models');
 
 
+router.get('/:id', async (req, res) => {
+    try {
+
+        
+      const NftData = await Nftitems.findByPk(req.params.id, {
+        
+        include: [{ model: NftAttributes}]
+      });
+  
+      if (!NftData) {
+        res.status(404).json({ message: 'No NFT found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(NftData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
-// router.post('/signup', async (req, res) => {
-//   try {
-//     const dbUserData = await User.create({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
+  router.get('/', async (req, res) => {
+    try {
+      const NftData = await Nftitems.findAll({
+        
+        include: [{ model: NftAttributes}]
+      });
+  
+      if (!NftData) {
+        res.status(404).json({ message: 'No NFT found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(NftData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
+  router.get('/query', async (req, res) => {
+    try {
 
-//       res.status(200).json(dbUserData);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
+       const trait_type = req.query.trait;
+       const value = req.query.value; 
+      const NftData = await Nftitems.findAll({
+        where: {
+            trait: trait_type,
+            value: value
+          },
+        include: [{ model: NftAttributes}]
+      });
+  
+      if (!NftData) {
+        res.status(404).json({ message: 'No results found for this query!' });
+        return;
+      }
+  
+      res.status(200).json(NftData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 
