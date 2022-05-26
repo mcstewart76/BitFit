3f3d5097b33be02a25e6decd3baebca7dad668f5
Ernesto Tham <ernesto.tham@kbslp.com>
2022-05-25 22:52:51 -0400

    updated DB

--- controllers/api/NFTRoutes.js
+++ controllers/api/NFTRoutes.js
@@ -68,5 +68,25 @@ router.get('/:id', async (req, res) => {
   });
 
 
+  router.post('/:id', async (req, res) => {
+    try {
+
+        
+      const NftData = await Nftitems.findByPk(req.params.id, {
+        
+        include: [{ model: NftAttributes}]
+      });
+  
+      if (!NftData) {
+        res.status(404).json({ message: 'No NFT found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(NftData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
